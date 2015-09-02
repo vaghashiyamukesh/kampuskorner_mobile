5306343c5170be783824c1669bd92544fd559120
Mukesh Vaghashiya <vaghashiya.mukesh@gmail.com>
2015-09-02 10:41:58 -0500

    removed js file

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,7 +6,6 @@ Rails.application.config.assets.precompile += %w(style.css)
 Rails.application.config.assets.precompile += %w(slider-styles.css)
 Rails.application.config.assets.precompile += %w(baskerville.css)
 Rails.application.config.assets.precompile += %w(slider.js)
-Rails.application.config.assets.precompile += %w(jquery.min.js )
 Rails.application.config.assets.precompile += %w(jquery-ui.min.js )
 Rails.application.config.assets.precompile += %w(jquery.slimscroll.min.js )
 
